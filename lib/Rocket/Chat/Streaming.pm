9030e8c030034438fd16bd67704bf1953a76ba6d
Justin Hawkins <justin@base64.com.au>
2018-09-21 15:34:25 +0930

    Start of the API

--- lib/Rocket/Chat/Streaming.pm
+++ lib/Rocket/Chat/Streaming.pm
@@ -2,7 +2,9 @@ use strict;
 use warnings;
 package Rocket::Chat::Streaming;
 
-use Mojo::Base;
+use Mojo::Base       qw/-base/;
+use Mojo::UserAgent;
+use Carp             qw/confess/;
 
 # ABSTRACT: An interface to the RocketChat streaming API
 
@@ -25,5 +27,31 @@ use Mojo::Base;
 =cut
 
 has 'url';
+has '_ua';
+
+=method connect
+
+Connect to the server.
+
+=cut
+
+sub connect {
+  my $self = shift;
+
+  confess "No url supplied to object" unless $self->url;
+
+  $self->_ua(Mojo::UserAgent->new);
+  $self->_ua->inactivity_timeout(60);
+
+  $self->_ua->websocket($self->url => sub {
+    my ($ua, $tx) = @_;
+    say 'WebSocket handshake failed!' and return unless $tx->is_websocket;
+    $tx->on(finish => sub {
+      my ($tx, $code, $reason) = @_;
+      say "WebSocket closed with status $code.";
+    });
+  });
+
+}
 
 1;
--- t/00-basic.t
+++ t/00-basic.t
@@ -1,5 +1,11 @@
 use Test::More;
+use Test::Exception;
 
 use_ok('Rocket::Chat::Streaming');
+
+my $rc = Rocket::Chat::Streaming->new;
+ok($rc, 'created object');
+
+throws_ok { $rc->connect } qr/no url supplied/i, 'cannot connect with no URL';
 
 done_testing();
